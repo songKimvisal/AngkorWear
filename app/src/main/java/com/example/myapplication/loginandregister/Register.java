8ad129bfca7f14f61a6db5ec13a3418f164489af
Song Kimvisal <songkimvisal21@gmail.com>
2025-03-12 21:39:06 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/loginandregister/Register.java
@@ -0,0 +1,51 @@
+package com.example.myapplication.loginandregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbarWithBackArrow);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) { // Check if getSupportActionBar() returns null
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+    public void alreadyHaveAccountOnClick(View view)
+    {
+        Intent intent = new Intent();
+        intent.setClass(this, login.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/loginandregister/login.java
@@ -0,0 +1,35 @@
+package com.example.myapplication.loginandregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.R;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+    public void RegisterOnClickBtn(View view)
+    {
+        Intent intent = new Intent();
+        intent.setClass(this, Register.class);
+        startActivity(intent);
+    }
+}
